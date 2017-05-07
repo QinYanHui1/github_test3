90897f8a5f803cffb08330bb843922221e6a3405
zhaoshuoshi <zhaoshuoshi@qq.com>
2017-05-07 22:22:55 +0800

    123

---
+++ src/main/java/com/example/SprngbootDemo1Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprngbootDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprngbootDemo1Application.class, args);
+	}
+}
